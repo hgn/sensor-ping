c792b32a9cd7a6181beb8e8fe0457b2bfd61029d
Hagen Paul Pfeifer <hagen@jauu.net>
2014-07-09 22:24:46 +0200

    integrate modules
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- ping.py
+++ ping.py
@@ -1,25 +1,77 @@
- 
-import os, sys, socket, struct, select, time
- 
+#!/usr/bin/env python
+
+import os
+import sys
+import socket
+import struct
+import select
+import time
+import json
+import pickle
+import requests
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+url = "http://localhost:8080/api/network/ping"
+headers = {'Content-type': 'application/json',
+            'Accept': 'application/vnd.sensor.jauu.net.v1+json'
+          }
+
 # From /usr/include/linux/icmp.h; your milage may vary.
 ICMP_ECHO_REQUEST = 8 # Seems to be the same on Solaris.
+
+FILE_PATH = "/tmp/foo.db"
+
+def db_append_data(new_data):
+
+    data = []
+    if os.path.isfile(FILE_PATH):
+        pkl_file = open(FILE_PATH, 'rb')
+        data = pickle.load(pkl_file)
+        pkl_file.close()
+
+    data.append(new_data)
+
+    output = open(FILE_PATH, 'wb')
+    pickle.dump(data, output)
+    output.close()
+
+    file = open(FILE_PATH, 'a+')
+    file.write("%s\n" % (data))
+    file.close()
+
+def db_append_data_arr(new_data):
+    for i in new_data:
+        db_append_data(i)
+
+
+def db_get_data():
+    if not os.path.isfile(FILE_PATH):
+        return None
+    pkl_file = open(FILE_PATH, 'rb')
+    data = pickle.load(pkl_file)
+    pkl_file.close()
+    return data
+
+
+def db_reset():
+    if os.path.isfile(FILE_PATH):
+        os.remove(FILE_PATH)
+
  
  
 def checksum(source_string):
-    """
-    I'm not too confident that this is right but testing seems
-    to suggest that it gives the same answers as in_cksum in ping.c
-    """
     sum = 0
-    countTo = (len(source_string)/2)*2
+    count_to = (len(source_string) / 2) * 2
     count = 0
-    while count<countTo:
-        thisVal = ord(source_string[count + 1])*256 + ord(source_string[count])
+    while count < count_to:
+        thisVal = ord(source_string[count + 1]) * 256 + ord(source_string[count])
         sum = sum + thisVal
-        sum = sum & 0xffffffff # Necessary?
+        sum = sum & 0xffffffff
         count = count + 2
  
-    if countTo<len(source_string):
+    if count_to < len(source_string):
         sum = sum + ord(source_string[len(source_string) - 1])
         sum = sum & 0xffffffff # Necessary?
  
@@ -27,17 +79,12 @@ def checksum(source_string):
     sum = sum + (sum >> 16)
     answer = ~sum
     answer = answer & 0xffff
- 
-    # Swap bytes. Bugger me if I know why.
     answer = answer >> 8 | (answer << 8 & 0xff00)
  
     return answer
  
  
 def receive_one_ping(my_socket, ID, timeout):
-    """
-    receive the ping from the socket.
-    """
     timeLeft = timeout
     while True:
         startedSelect = time.time()
@@ -63,25 +110,17 @@ def receive_one_ping(my_socket, ID, timeout):
  
  
 def send_one_ping(my_socket, dest_addr, ID):
-    """
-    Send one ping to the given >dest_addr<.
-    """
     dest_addr  =  socket.gethostbyname(dest_addr)
  
-    # Header is type (8), code (8), checksum (16), id (16), sequence (16)
     my_checksum = 0
  
-    # Make a dummy heder with a 0 checksum.
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, my_checksum, ID, 1)
     bytesInDouble = struct.calcsize("d")
     data = (192 - bytesInDouble) * "Q"
     data = struct.pack("d", time.time()) + data
  
-    # Calculate the checksum on the data and the dummy header.
     my_checksum = checksum(header + data)
  
-    # Now that we have the right checksum, we put that in. It's just easier
-    # to make up a new header than to stuff it into the dummy.
     header = struct.pack(
         "bbHHh", ICMP_ECHO_REQUEST, 0, socket.htons(my_checksum), ID, 1
     )
@@ -115,29 +154,76 @@ def do_one(dest_addr, timeout):
     return delay
  
  
-def verbose_ping(dest_addr, timeout = 2, count = 4):
-    """
-    Send >count< ping to >dest_addr< with the given >timeout< and display
-    the result.
-    """
+def ping(dest_addr, timeout = 10, count = 5):
+    res = list()
     for i in xrange(count):
         print "ping %s..." % dest_addr,
         try:
             delay  =  do_one(dest_addr, timeout)
         except socket.gaierror, e:
             print "failed. (socket error: '%s')" % e[1]
-            break
+            res.append(0)
  
         if delay  ==  None:
             print "failed. (timeout within %ssec.)" % timeout
+            res.append(0)
         else:
-            delay  =  delay * 1000
-            print "get ping in %0.4fms" % delay
-    print
- 
- 
+            delay  =  int(delay * 1000)
+            print "get ping in %dms" % delay
+            res.append(delay)
+
+    return res
+
+
+def construct_data(host, measurement_data):
+    data = dict()
+    data["host"]      = host
+    data["times"]     = measurement_data
+    data["timestamp"] = int(time.time())
+    return data
+
+
+def sync_data(all_data):
+    failed_sync_data = []
+    for i in all_data:
+        try:
+            r = requests.post(url, data=json.dumps(i), headers=headers)
+        except :
+            failed_sync_data.append(i)
+            continue
+        print("Status Code:")
+        print(r.status_code)
+        print("")
+        print("URL:")
+        print(r.url)
+        print("")
+        print("Encoding:")
+        print(r.encoding)
+        print("")
+        print("Content:")
+        print(r.content)
+        print("")
+        if int(r.status_code) != 202:
+            print("Failed to sync data")
+            failed_sync_data.append(i)
+            continue
+
+    return failed_sync_data
+
+
+def main(host):
+    res = ping(host)
+
+    json_data = construct_data(host, res)
+    db_append_data(json_data)
+
+    all_data = db_get_data()
+    remain_data = sync_data(all_data)
+    db_reset()
+    if len(remain_data) > 0:
+        print("Not all synced, save to DB")
+        db_append_data_arr(remain_data)
+
+
 if __name__ == '__main__':
-    verbose_ping("heise.de")
-    verbose_ping("google.com")
-    verbose_ping("a-test-url-taht-is-not-available.com")
-    verbose_ping("192.168.1.1")
+    main("localhost")
